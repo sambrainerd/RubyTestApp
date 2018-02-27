e979938c05ac5a95019497f6b23452a696a153e9
Brainerd <samuelb@amazon.com>
2018-02-27 12:23:03 +0530

    completed form for announcements with submit button

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -2,4 +2,16 @@ class AnnouncementsController < ApplicationController
   def new
     @announcement = Announcement.new
   end
+
+  def create
+    #render plain: params[:announcement].inspect
+    @announcement = Announcement.new(announcement_params)
+    @announcement.save
+    redirect_to announcements_show(@announcement)
+  end
+
+  private
+    def announcement_params
+      params.required(:announcement).permit(:announcement,:details,:priority)
+    end
 end
