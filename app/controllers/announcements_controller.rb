81e9428eae4427208b0007561b24085cfaff8342
Brainerd <samuelb@amazon.com>
2018-02-28 10:22:23 +0530

    added a show view and error messages if validation fails. Created button on show view to create new announcement.

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -4,10 +4,17 @@ class AnnouncementsController < ApplicationController
   end
 
   def create
-    #render plain: params[:announcement].inspect
     @announcement = Announcement.new(announcement_params)
-    @announcement.save
-    redirect_to announcements_show(@announcement)
+    if @announcement.save
+      flash[:notice] = "Announcement was successfully posted"
+      redirect_to announcement_path(@announcement)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @announcement = Announcement.find(params[:id])
   end
 
   private
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,5 +1,5 @@
 class Announcement < ActiveRecord::Base
-   validates :announcement, presence:true, length: {minimum:10, maximum:50}
-   validates :details, presence:true, length: {minimum:10, maximum:300}
+   validates :announcement, presence:true, length: {minimum:10, maximum:100}
+   validates :details, presence:true, length: {minimum:10, maximum:500}
    validates :priority, presence:true, length: {minimum:3, maximum:6}
 end
